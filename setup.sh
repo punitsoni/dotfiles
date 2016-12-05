24baf45f23c1a4d583fd7c7ea9ddb17054017ff8
Punit Soni <punitxsmart@gmail.com>
2016-12-05 01:18:10 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,18 +51,6 @@ overwrite_check "~/.bashrc and ~/.dotfiles" && {
     ln -sf $HOME/.dotfiles/bashrc $HOME/.bashrc
 }
 
-: '
-# do not setup vim until portability issues are fixed
-echo "configuring vim..."
-overwrite_check "~/.vimrc and ~/.vim" && {
-    # TODO: move the original vimrc if its not a symlink
-    if [ ! -h $HOME/.vimrc ]; then
-        mv $HOME/.vimrc $HOME/.vimrc_default.vim
-    fi
-    ln -sf $HOME/.dotfiles/vimrc $HOME/.vimrc;
-    ln -sf $HOME/.dotfiles/vim $HOME/.vim;
-}
-'
 echo "configuring spacemacs..."
 overwrite_check "$HOME/.spacemacs" && {
     ln -sf $HOME/.dotfiles/spacemacs $HOME/.spacemacs
