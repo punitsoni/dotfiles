56246a0ca1eb33a3728e73bf310591aea6b9ebd4
Punit Soni <punitxsmart@gmail.com>
2019-01-14 18:00:38 -0800

    Setup dev branch for fresh new development.
    Move old stuff to archive.

--- bashrc_linux.sh
+++
@@ -1,4 +0,0 @@
-# --- Linux-specific BASH settings ---
-
-# setup dircolors
-#eval `dircolors $DOTFILES_DIR/dircolors`
--- bashrc_mac.sh
+++
@@ -1,5 +0,0 @@
-# --- mac-specific bash settings ---
-alias ls='ls -G'
-alias la='ls -al'
-# force 256 color terminal in tmux to work with solarized theme
-# alias tmux="TERM=xterm-256color /opt/local/bin/tmux"
