1ed9db91269342c981ecfbae5bb41a50830860f4
Punit Soni <punitxsmart@gmail.com>
2020-02-27 17:00:02 -0800

    More i3 stuff
    polybar config
    compton config
    lockscreen

--- i3/screenlock.sh
+++ i3/screenlock.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 export XSECURELOCK_AUTH_FOREGROUND_COLOR=green
-export XSECURELOCK_FONT=monospace:size=20
+# export XSECURELOCK_FONT=monospace:size=20
 export XSECURELOCK_PASSWORD_PROMPT=time_hex
-exec xsecurelock
+export XSECURELOCK_BLANK_TIMEOUT=-1
+export XSECURELOCK_SAVER=saver_xscreensaver
+
+exec /usr/share/goobuntu-desktop-files/xsecurelock.sh
+
+# xsecurelock
