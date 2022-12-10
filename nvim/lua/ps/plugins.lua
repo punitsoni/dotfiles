57590e3afdfcd4a17930fb27904ebce7002ce5db
Punit Soni <punitxsmart@gmail.com>
2022-12-09 17:29:59 -0800

    nvim

--- nvim/lua/ps/plugins.lua
+++ nvim/lua/ps/plugins.lua
@@ -28,9 +28,10 @@ require('packer').startup(function(use)
   -- Configurations for neovim lsp
   use 'neovim/nvim-lspconfig'
 
-  -- Completion engine
+  -- Completion engine and sources.
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lua'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
