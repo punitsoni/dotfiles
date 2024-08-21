769a4521152ceed65f98c825dec9a7ffe4b14d26
Punit Soni <punitxsmart@gmail.com>
2024-08-21 10:45:09 -0700

    add shell function for fzf ripgrep integration

--- sh/functions.sh
+++ sh/functions.sh
@@ -81,6 +81,34 @@ ff() {
   [[ ! -z $file ]] && $EDITOR $file
 }
 
+# ripgrep->fzf->vim [QUERY]
+# Requires `bat` --> brew install bat
+frg() {
+  dir=$1
+
+  if [[ -z $dir ]]; then
+    dir=.
+  fi
+
+  RELOAD="reload:rg --column --color=always --smart-case {q} ${dir}|| :"
+  OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
+            ${EDITOR} {1} +{2}     # No selection. Open the current line in Vim.
+          else
+            ${EDITOR} +cw -q {+f}  # Build quickfix list for the selected items.
+          fi'
+  fzf --disabled --ansi --multi \
+      --bind "start:$RELOAD" --bind "change:$RELOAD" \
+      --bind "enter:become:$OPENER" \
+      --bind "ctrl-o:execute:$OPENER" \
+      --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+      --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+      --preview-window '~4,+{2}+4/3,<80(up)' \
+      --delimiter : \
+      --query ""
+
+      # --delimiter / --with-nth -1 \
+}
+
 
 # Set terminal cursor shape.
 cursor() {
@@ -124,4 +152,5 @@ random_chuck_joke() {
 weather() {
   curl "wttr.in?q0uFnM";
 }
+
 
