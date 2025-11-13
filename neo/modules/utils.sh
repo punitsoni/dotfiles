036b2fb47b933249c8267b22ff8fcfe6fb262080
Punit Soni <punitxsmart@gmail.com>
2025-11-13 11:02:03 -0800

    neo newkey

---
+++ neo/modules/utils.sh
@@ -0,0 +1,12 @@
+
+
+
+# Displays weather in your current location.
+cmd::newkey() {
+	ssh-keygen -t ed25519 -C "-- Punit Soni --"
+	echo "new key generated"
+	echo ""
+	cat ~/.ssh/id_ed25519.pub
+	echo ""
+}
+
